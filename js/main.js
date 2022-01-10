46e0f340c0008a4a43b1bea066ce7b5f905f2e85
Jia Song <jiasong214@gmail.com>
2022-01-10 21:55:59 +1100

    add a checkWin logic

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const board = {
   gameBoard: [],
 
   // create a game board depends on arguments value
-  initialise: function(column, row) {
-    for(let i=0; i<column; i++) {
-      let rowArr = new Array(row).fill("");
+  initialise: function(size) {
+    for(let i=0; i<size; i++) {
+      let rowArr = new Array(size).fill("");
     
       this.gameBoard.push(rowArr);
     }
@@ -23,11 +23,17 @@ const board = {
 
   // represent the board in DOM
   drawDOM: function() {
-    // clean the DOM first
-    $("#board").html("");
+    // clean the DOM, layout grid
+    $("#board")
+      .html("")
+      .css({
+      "grid-template-columns": `repeat(${this.gameBoard.length}, 1fr)`,
+      "grid-template-rows": `repeat(${this.gameBoard.length}, 1fr)`
+    });
 
+    // append items in board
     for(let i=0; i<this.gameBoard.length; i++) {
-      for(let j=0; j<this.gameBoard[i].length; j++) {
+      for(let j=0; j<this.gameBoard.length; j++) {
         $("#board").append(`
           <div class="${i}-${j}">
             <span>${this.gameBoard[i][j]}</span>
@@ -43,7 +49,7 @@ const board = {
   },
 }
 
-board.initialise(3,3);
+board.initialise(3);
 board.drawDOM();
 
 // 2. set players
@@ -108,12 +114,103 @@ const game = {
   checkWin: function() {
     const currentBoard = board.gameBoard;
 
+    let ref = currentBoard[0][0];
+    let counter = 0;
+
+    // 1. check rows
     for(let i=0; i<currentBoard.length; i++) {
-      for(let j=0; j<currentBoard[i].length; j++) {
-        // ...
+      for(let j=0; j<currentBoard.length; j++) {
+        if(ref === "") {
+          continue;
+        }
+  
+        if(ref === currentBoard[i][j]) {
+          counter++;
+
+          if(counter === 3) {
+            console.log(`there is winner in column ${i}, row ${j}`);
+            console.log(`[${i}, ${j-2}], [${i}, ${j-1}], [${i}, ${j}]`);
+          }
+        }else {
+          counter = 0;
+          ref = currentBoard[i][j];
+        }
+  
+        // console.log(currentBoard)
+        // console.log(counter);
       }
     }
 
+    ref = currentBoard[0][0];
+
+    // 2. check columns
+    for(let i=0; i<currentBoard.length; i++) {
+      for(let j=0; j<currentBoard.length; j++) {
+        if(ref === "") {
+          continue;
+        }
+  
+        if(ref === currentBoard[j][i]) {
+          counter++;
+
+          if(counter === 3) {
+            console.log(`there is winner in column ${j}, row ${i}`);
+            console.log(`[${i}, ${j-2}], [${i}, ${j-1}], [${i}, ${j}]`);
+          }
+        }else {
+          counter = 0;
+          ref = currentBoard[j][i];
+        }
+  
+        console.log(currentBoard)
+        console.log(counter);
+      }
+    }
+
+    ref = currentBoard[0][0];
+
+
+    // 3. check diagonal (1)
+    for(let i=0; i<currentBoard.length; i++) {
+      if(ref === "") {
+        continue;
+      }
+  
+      if(ref === currentBoard[i][i]) {
+        counter++;
+
+        if(counter === 3) {
+          console.log(`there is winner in column ${i}, row ${i}`)
+        }
+      }else {
+        counter = 0;
+        ref = currentBoard[i][i];
+      }
+    }
+
+    ref = currentBoard[0][0];
+
+
+    // 4. check diagonal (2)
+    for(let i=0; i<currentBoard.length; i++) {
+      if(ref === "") {
+        continue;
+      }
+
+      console.log(currentBoard.length-i, i)
+  
+      if(ref === currentBoard[currentBoard.length-i-1][i]) {
+        counter++;
+
+        if(counter === 3) {
+
+          console.log(`there is winner in column ${currentBoard.length-i-1}, row ${i}`)
+        }
+      }else {
+        counter = 0;
+        ref = currentBoard[currentBoard.length-i-1][i];
+      }
+    }
   },
 
   clickBoard: function(boardID) {
